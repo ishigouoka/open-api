c02e898899c9cc9a2f7a8f1f78c9fa2ec18f6c0e
jpx7416 <Line!ca1>
2021-07-08 17:07:33 +0900

    topic/generated_server

--- generateServerInterface.sh
+++ generateServerInterface.sh
@@ -6,6 +6,6 @@ cd ./generated-server
 # 利用可能な言語は以下コマンドで確認
 # ./node_modules/@openapitools/openapi-generator-cli/bin/openapi-generator list
 
-../node_modules/@openapitools/openapi-generator-cli/bin/openapi-generator generate -i ../api-spec/work/index.yaml -g spring -c ../api-spec/config_server.yaml
+../node_modules/@openapitools/openapi-generator-cli/bin/openapi-generator generate -i ../api-spec/work/index.yaml -g kotlin-spring -c ../api-spec/config_server.yaml
 
 echo "finish... Generated Server Interface."
